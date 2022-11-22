781c02ad29d8ae725b7f0fb9a5dbddc3a4bd6a85
haidoe <haidren.amalia@gmail.com>
2022-11-22 15:14:43 -0800

    Homepage - Added Alt Text and Form Updates

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,3 +2,6 @@ menuBtn.addEventListener("click", () => {
   menuBtn.classList.toggle("open");
   nav.classList.toggle("show");
 });
+
+searchForm.addEventListener("submit", (e) => e.preventDefault());
+newsletterForm.addEventListener("submit", (e) => e.preventDefault());
